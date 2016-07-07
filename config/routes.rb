cf431ce7d836d82c048ba0b656353eab64d7f999
Leo Le <coder.leo.le@gmail.com>
2016-07-07 07:40:51 +0700

    order landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/contacts', to: 'contacts#index'
   get '/menus', to: 'menus#index'
+  get '/orders', to: 'orders#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
