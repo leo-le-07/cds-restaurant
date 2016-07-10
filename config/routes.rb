22c7bf5469b5d9eafa15591b230b9040b1279908
Leo Le <coder.leo.le@gmail.com>
2016-07-10 07:44:09 +0700

    Add view count function

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'order_details/add', to: 'order_details#add'
 
   root 'home#index'
-  get '/items', to: 'items#index'
   get '/contacts', to: 'contacts#index'
   get '/menus', to: 'menus#index'
   resources :orders
+  resources :items
 end
